59bec092e0fdc5a5ed91fec846e97bb767bc0551
jacquibushfi <jacqui@stainedglassofmckinney.com>
2019-11-11 14:21:20 -0600

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  attr_accessor :name, :breed, :age
+  DOGS = []
+
+  def initialization(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
+  end
+
+  def self.all
+    DOGS
+  end
+
+
+end
